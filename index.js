b3e6498b2c8dd94f9e06185174e8b6af405ffb42
Mohammed Ali Jaafar <mohammedalijaafar1999@gmail.com>
2021-07-15 12:19:20 +0300

    starter files with swup for page transtion

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index');
+})
+
+app.get('/works', (req, res) => {
+  res.render('works');
+})
+
+app.get('/about', (req, res) => {
+  res.render('about');
+})
+
+app.get('/blog', (req, res) => {
+  res.render('blog');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
